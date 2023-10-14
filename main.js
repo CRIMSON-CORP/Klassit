9e40fe826fa5bbdba427570bd9c5ae963f20e9ee
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-10-14 13:54:49 +0100

    made 5 more sections including faq

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-import './style.css'
+import "./style.css";
+
+function accordions() {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((accordion) => {
+    accordion.onclick = (e) => {
+      const accordionOpen = accordion.dataset.accordionOpen === "true";
+      if (accordionOpen) {
+        accordion.children[1].style.maxHeight = "0px";
+        accordion.setAttribute("data-accordion-open", false);
+      } else {
+        accordion.children[1].style.maxHeight = `${accordion.children[1].scrollHeight}px`;
+        accordion.setAttribute("data-accordion-open", true);
+      }
+    };
+  });
+}
+
+accordions();
