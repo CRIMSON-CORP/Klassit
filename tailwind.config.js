15916db4c09f6803ae05734cf6eec00dfc0e420f
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-10-14 02:31:46 +0100

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1E3E2B',
+        secondary: '#D0EA51',
+        tertiary:'#0BA94B'
+      },
+      fontFamily: {
+        'body': ['Open Sans', 'sans-serif']
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        'DEFAULT': '20px',
+      }
+    }
   },
   plugins: [],
 }
