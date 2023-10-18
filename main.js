a0dcbe5bcdca76e612c1f8edd05d40a229c61901
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-10-19 00:11:17 +0100

    animated about page

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollTrigger.normalizeScroll();
 
 let pageLoaded = false;
-const page = window.location.pathname.substring(1);
+const page = window.location.pathname.substring(1).split(".")[0];
 
 ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
@@ -624,15 +624,45 @@ function joinWaitlist() {
     });
 }
 
+function aboutHero() {
+  const timeline = gsap.timeline();
+
+  timeline.from("#hero span", { opacity: 0, y: "150%" }).to("#hero h1 .word", {
+    y: 0,
+    opacity: 1,
+    stagger: { each: 0.08 },
+    ease: "back.out(1.4)",
+    duration: 1.5,
+  });
+}
+
+function aboutContentTimeline() {
+  const paragraphs = document.querySelectorAll(".about p .line");
+
+  paragraphs.forEach((paragraph) => {
+    gsap.to(paragraph, {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      ease: "power3.out",
+      duration: 1,
+      stagger: { each: 0.1, from: "start" },
+      scrollTrigger: {
+        trigger: paragraph,
+        start: "bottom 80%",
+      },
+    });
+  });
+}
+
 function main() {
-  console.log("main");
   toggleMobileNav();
+  headerAnimation();
 
   if (page === "") {
     accordions();
     currentYear();
 
-    headerAnimation();
     heroAnimation();
     servicesAnimation();
     whoCanUse();
@@ -642,6 +672,10 @@ function main() {
     groupClass();
     faq();
     joinWaitlist();
+  }
+  if (page === "about") {
+    aboutHero();
+    aboutContentTimeline();
   }
 }
 
